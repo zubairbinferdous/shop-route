75f56df9ec32d5900e4c8e1478c747a192ee020c
zubairbinferdous <zubidev71@gmail.com>
2023-08-21 22:00:56 +0600

    ok

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+
+function App() {
+  return (
+    <>
+      <h1>this is my page of design</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, ad.
+        Fugit, voluptatum beatae explicabo commodi accusamus modi in amet rerum
+        sequi porro itaque unde. Ab minus dignissimos perferendis eius amet
+        tenetur, vel numquam nobis est consectetur earum esse maxime. Alias
+        quaerat illo tempore consequuntur? Consectetur porro ab delectus enim
+        cupiditate libero magnam commodi corrupti, animi accusantium possimus
+        aliquid. Adipisci illum soluta saepe qui cum vel nihil, ab suscipit
+        exercitationem? Tenetur fuga corrupti eos possimus doloremque maiores
+        reiciendis quis. Ad, beatae quae corrupti omnis exercitationem fugiat,
+        tenetur minus mollitia enim libero rem, magnam deleniti inventore
+        adipisci quasi dolorum a aspernatur consectetur?
+      </p>
+
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, quam!</p>
+      
+    </>
+  );
+}
+
+export default App;
