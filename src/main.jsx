350d2f0d2995bc646a1b546bd35aebd09e8f64f8
zubairbinferdous <zubidev71@gmail.com>
2023-08-25 05:42:32 +0600

    ok done

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,7 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Home></Home>,
+        loader: () => fetch ('shirt.json') 
       },
       {
         path: '/about',
